042030adebac1ae6f715615e235218ad1db121bb
ar-insect <ar.insect@gmail.com>
2014-06-18 19:46:26 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,90 +14,219 @@ module.exports = function(grunt) {
 		banner : '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 		// Task configuration.
 		copy : {
+			/*
+			move : {
+				files : [{
+					expand : true,
+					cwd : './',
+					src : ['enterprise/**'],
+					dest : 'sea-modules'
+				}]
+			},
+			*/
+			/*
+			 * assets 为静态文件的目录，存放编译打包后的js&css
+			 */
+			$ : { // jQuery 默认请求sea-modules下的 $
+				files : [{
+					expand : true,
+					cwd : 'lib/',
+					src : ['$.js'],
+					dest : 'sea-modules'
+				}]
+			},
+			arale : { // 为了避免解析依赖模块错误，需要事先将依赖的组件模块cp到项目根目录下
+				files : [{
+					expand : true,
+					cwd : 'sea-modules/arale/',
+					src : ['dialog/1.2.6/*', 'calendar/1.0.0/*'], // 追加arale组件
+					dest : 'arale'
+				}]
+			},
+			// 自定义插件，放在公共目录下面
+			select : {
+				files : [{
+					expand : true,
+					cwd : 'lib/',
+					src : ['select/1.0.0/*'],
+					dest : 'assets/js/common'
+				}]
+			},
 			cellula : {
 				files : [{
 					expand : true,
 					cwd : 'lib/',
-					src : ['cellula/**'],
-					dest : 'dest/'
+					src : ['cellula/0.4.1/*.js'],
+					dest : 'assets/js/common'
+				}]
+			},
+			fdp : {
+				files : [{
+					expand : true,
+					cwd : 'lib/',
+					src : ['fdp/1.0.0/*.js'],
+					dest : 'assets/js/common'
 				}]
 			}
 		},
 		transport : {
 			options : {
+				debug : false,
 				alias : '<%= pkg.alias %>',
 				parsers : {
-					'.js' : [script.jsParser]
+					'.js' : [script.jsParser],
+					'.css' : [style.css2jsParser]
 				},
 				paths : ['.']
 			},
-			cellula : {
+			select : {
 				options : {
-					idleading : 'dest/cellula/'
+					idleading : 'assets/js/common/select/1.0.0/'
 				},
 				files : [{
 					expand : true,
 					filter : 'isFile',
-					cwd : 'lib/cellula',
-					src : '**/*.js',
-					dest : 'dest/cellula'
+					cwd : 'lib/select/1.0.0',
+					src : '*.js',
+					dest : 'assets/js/common/select/1.0.0'
 				}]
 			},
-			cellula_build : {
+			cellula : {
 				options : {
-					idleading : 'build/cellula/'
+					idleading : 'assets/js/common/cellula/0.4.1/'
 				},
 				files : [{
 					expand : true,
 					filter : 'isFile',
-					cwd : 'lib/cellula',
-					src : '**/cellula.js',
-					dest : 'build/cellula'
+					cwd : 'lib/cellula/0.4.1',
+					src : '*.js',
+					dest : 'assets/js/common/cellula/0.4.1'
+				}]
+			},
+			fdp : {
+				options : {
+					idleading : 'assets/js/common/fdp/1.0.0/'
+				},
+				files : [{
+					expand : true,
+					filter : 'isFile',
+					cwd : 'lib/fdp/1.0.0',
+					src : '*.js',
+					dest : 'assets/js/common/fdp/1.0.0'
+				}]
+			},
+			itemList : {
+				options : {
+					idleading : 'assets/js/itemList/0.1.0/'
+				},
+				files : [{
+					expand : true,
+					filter : 'isFile',
+					cwd : 'static/js/itemList/0.1.0',
+					src : '*.js',
+					dest : 'assets/js/itemList/0.1.0'
+				}]
+			},
+			singleForm : {
+				options : {
+					idleading : 'assets/js/singleForm/1.0.0/'
+				},
+				files : [{
+					expand : true,
+					filter : 'isFile',
+					cwd : 'static/js/singleForm/1.0.0',
+					src : '*.js',
+					dest : 'assets/js/singleForm/1.0.0'
+				}]
+			},
+			searchingScene : {
+				options : {
+					idleading : 'assets/js/searchingScene/1.0.0/'
+				},
+				files : [{
+					expand : true,
+					filter : 'isFile',
+					cwd : 'static/js/searchingScene/1.0.0',
+					src : '*.js',
+					dest : 'assets/js/searchingScene/1.0.0'
 				}]
 			}
 		},
+		cssmin: {
+	         options: {
+	         	//keepSpecialComments: 0
+	         },
+	         compress: {
+	             files: {
+	                 'assets/css/common/base/1.0.0/base.css': ['static/css/base/1.0.0/*.css'],
+	                 'assets/css/common/enterpriseportal/1.0.0/enterpriseportal.css': ['static/css/enterprise/1.0.0/*.css']
+	             }
+	         }
+     	},
 		concat : {
 			options : {
-				paths : ['.'],
-				include : 'relative'
-				//banner : '<%= banner %>',
-				//stripBanners : true
+				paths : ['.']
 			},
-			cellula : {
-				options : {
-					include : 'all'
-				},
+			cellula: {
+				files : {
+					'assets/js/common/cellula/0.4.1/cellula.js': ['assets/js/common/cellula/0.4.1/*.js']
+				}
+			},
+			fdp : {
+				files : {
+					'assets/js/common/fdp/1.0.0/fdp.js': ['assets/js/common/fdp/1.0.0/*.js']
+				}
+			},
+			itemList: {
+			}
+		},
+		/*
+		uglify : {
+			options : {
+				//banner : '<%= banner %>',
+				mangle : false
+			},
+			all : {
 				files : [{
 					expand : true,
 					cwd : 'build/',
-					src : ['cellula/**/cellula.js'],
-					dest : 'build/',
-					ext : '.js'
+					src : '.js',
+					dest : 'build/min'
 				}]
-			}
-		},
-		uglify : {
-			options : {
-				banner : '<%= banner %>',
-				mangle: false
-			},
-			dist : {
-				src : 'build/cellula/0.4.1/cellula.js',
-				dest : 'build/cellula/0.4.1/<%= pkg.name %>.min.js'
 			},
 		},
+		*/
+		clean : {
+			temp : ['arale', 'alipay', 'gallery', 'enterprise']
+		}
 	});
-	
+
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-cmd-transport');
 	grunt.loadNpmTasks('grunt-cmd-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	// grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	// grunt.loadNpmTasks('grunt-contrib-jshint');
 	// grunt.loadNpmTasks('grunt-contrib-watch');
-
 	// Default task.
-	grunt.registerTask('default', ['copy', 'transport', 'concat']);
+	grunt.registerTask('enterprise', [
+		'copy:$', 
+		'copy:arale', 
+		'copy:select',
+		'copy:cellula',
+		'copy:fdp',
+		'transport:select', 
+		'transport:cellula',
+		'transport:fdp',
+		'transport:itemList',
+		'transport:singleForm',
+		'transport:searchingScene',
+		'concat',
+		'cssmin', 
+		'clean'
+	]);
 
 };
--- README.md
+++ README.md
@@ -1,26 +1,85 @@
-## ui-library 场景模块库 ##
+## ui library ##
 
-----------
+---
 
-构建工具：grunt
+**交互场景库**
 
-目录结构：
+由于涉及组件和场景较多，正在陆陆续续更新中...
 
--- `build` 合并压缩
+本地构建环境： `node v0.10.26` | `npm 1.4.3` | `grunt 0.4.5`
 
--- `dest` 提取依赖的模块
+构建方法
+--
+1. 将项目克隆到本地：`git clone git@github.com:ar-insect/ui-library.git`
 
--- `example` 示例
+2. 切换到项目根目录下面，比如：`cd ~/enterprise`
 
--- `lib` 公共js库
+3. 执行 `npm install` 安装项目所需要的插件
 
--- `node_modules` grunt插件模块
+4. 本地开发完成后在`gruntfile.js`里面配置部署（依赖配置在`package.json`的`alias`）
 
--- `static` 静态资源
+5. 在项目的根目录执行 `grunt enterprise` 完成静态文件的编译和打包
 
--- `test` 单元测试
+6. 最后别忘记在本地测试ok提交代码哦~
 
-Gruntfile.js
+Q&A
+--
+- 问：有没有案例可以参考一下呢？
 
-package.js
+答： 看完 example/itemList.htm 你基本上就知道了：）
 
+- 问：如何在项目中安装arale组件呢？
+
+答：很简单，比如需要安装`dialog`组件，进入项目根目录中执行`spm install arale.dialog`就会自动下载组件以及其依赖到`sea-modules'下面，具体可以访问`arale.org`
+注意：以上操作仅限于在内网中使用，然后你在项目中这么引用 `require('arale/dialog/1.2.6/dialog.js')`
+
+目录结构
+--
+
+/-- `example` 示例
+
+/-- `assets` 静态文件资源库（存放编译打包后的js&css）
+
+	/-- js
+		
+		/-- common 公共模块
+
+			/-- cellula
+				
+			/-- fdp `Form` `dataView` `paginator`
+
+			/-- select
+		
+		/-- itemList 一个使用`cellula`做的小玩意儿
+		
+		/-- singleForm 单表单模型
+		
+		/-- searchingScene 异步查询场景模型
+
+	/-- css
+
+		/-- common
+
+			/-- base
+
+			/-- enterpriseportal
+			
+			/-- ...
+
+
+/-- `lib` 公共js库
+
+/-- `sea-modules` sea模块
+ 			
+	 /-- .. arale组件等
+
+/-- `static` 静态资源
+
+/-- `test` 单元测试
+
+Gruntfile.js 部署脚本
+
+package.js 项目配置
+
+
+如有问题或建议请 ar.insect@gmail.com
